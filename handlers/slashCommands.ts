adb24676008e7e06f7a52f32c19b5fce89c7f0ff
dhfhfk <dhfhfk1203@gmail.com>
2022-06-24 23:08:46 +0900

    feat: 개발자 명령어

--- handlers/slashCommands.ts
+++ handlers/slashCommands.ts
@@ -1,20 +1,30 @@
 import { Client, ApplicationCommandDataResolvable } from "discord.js";
+import config from "../config";
 import fs from "fs";
 
 module.exports = async (client: Client) => {
     // 빗금 명령어
     const slashCommands = fs.readdirSync("./commands");
     const arrayOfSlashCommands: ApplicationCommandDataResolvable[] = [];
+    const arrayOfDevCommands: ApplicationCommandDataResolvable[] = [];
 
     slashCommands.map((value: string) => {
         import(`../commands/${value.replace(/.ts | .js/g, "")}`).then(async (command) => {
             if (["MESSAGE", "USER"].includes(command.type)) delete command.description;
-            arrayOfSlashCommands.push(command);
+            if (command.dev) {
+                arrayOfDevCommands.push(command);
+            } else {
+                arrayOfSlashCommands.push(command);
+            }
         });
     });
 
     client.on("ready", async () => {
-        // await client.guilds.cache.get("길드 커맨드 셋을 위한 Guild Id")?.commands.set(arrayOfSlashCommands);
+        if (config.dev_guilds) {
+            config.dev_guilds.forEach(async (guild) => {
+                await client.guilds.cache.get(guild)?.commands.set(arrayOfDevCommands);
+            });
+        }
 
         // 전역 커맨드 셋
         await client.application?.commands.set(arrayOfSlashCommands);
