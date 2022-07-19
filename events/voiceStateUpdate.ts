f62be9fc0be3583b1413ce197aaf45acd4a92e3c
dhfhfk <dhfhfk1203@gmail.com>
2022-07-19 15:37:25 +0900

    test: 필요없는 event 파일 제거

--- events/voiceStateUpdate.ts
+++
@@ -1,7 +0,0 @@
-import { VoiceState } from "discord.js";
-import client from "../bot";
-import Settings from "../models/guild-settings";
-
-client.on("voiceStateUpdate", async (oldState: VoiceState, newState: VoiceState) => {
-    // voiceStateUpdate
-});
