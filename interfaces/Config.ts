ac3afedea2d0ca540ac3ed3b1213ba41fd6f17a3
dhfhfk <dhfhfk1203@gmail.com>
2022-07-30 00:02:53 +0900

    fix: Config interface

---
+++ interfaces/Config.ts
@@ -0,0 +1,8 @@
+export interface Config {
+    token: string;
+    mongodb_uri: string;
+    languages: string[];
+    dev_guilds: string[];
+    dev_users: string[];
+    koreanbots_token: string;
+}
